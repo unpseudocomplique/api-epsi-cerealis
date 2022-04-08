cac39fb4ea7c0d37b1f02be4df2c91ba955477ed
Mathieu <mathieu@bestconsultant.fr>
2022-04-08 13:37:43 +0100

    init nuxt app with api cre

---
+++ server/api/createUser.ts
@@ -0,0 +1,22 @@
+import { useBody, useCookies, useQuery } from 'h3'
+import * as hubspot from '@hubspot/api-client';
+
+const hubspotClient = new hubspot.Client({
+    apiKey: 'eu1-7270-c62b-424b-a5a3-0c815a4bffcc',
+});
+
+
+
+export default async (req) => {
+    const body = await useBody(req)
+    const { firstname, lastname } = body
+    const contactObj = {
+        properties: {
+            firstname, lastname
+        },
+    };
+    const createContactResponse = await hubspotClient.crm.contacts.basicApi.create(contactObj);
+    console.log('createContactResponse', createContactResponse);
+
+    return { createdUser: createContactResponse }
+}
